44436f966910d978b309e462d3843db2ec7c77db
Štěpán Tomsa <stomsa@redhat.com>
2018-12-09 13:34:59 +0100

    Fix the Flask application root_path
    If __name__ is used as the application’s import name, its root_path is
    set to /app instead of just /. This will make trouble later when
    adding Connexion. Changed to "demo" which doesn’t correspond to any
    module.

--- app/flask.py
+++ app/flask.py
@@ -3,6 +3,6 @@ from flask import Flask
 
 
 def create_app():
-    flask_app = Flask(__name__)
+    flask_app = Flask('demo')
     flask_app.add_url_rule("/", view_func=index)
     return flask_app
--- app/prometheus.py
+++ app/prometheus.py
@@ -5,7 +5,7 @@ from werkzeug.wsgi import DispatcherMiddleware
 
 
 def create_app():
-    flask_app = Flask(__name__)
+    flask_app = Flask('demo')
     flask_app.add_url_rule("/", view_func=index)
 
     prometheus_app = make_wsgi_app()
